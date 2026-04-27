2115553eb0f7e6c1dcefdd33b48af1ba43f6b3c3
maygo <johann@guinberteau.fr>
2026-04-27 10:39:53 +0200

    🎉 Maven Project

---
+++ src/main/java/fr/baptouk/pokerixe/backend/PokerixeBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.baptouk.pokerixe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerixeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerixeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/baptouk/pokerixe/backend/PokerixeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.baptouk.pokerixe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerixeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
