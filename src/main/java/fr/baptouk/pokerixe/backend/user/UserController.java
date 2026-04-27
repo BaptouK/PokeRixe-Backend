36981b2375e8b40e60a06d69f294f7c9e9d3490a
Johann G. <52964957+MaygoDev@users.noreply.github.com>
2026-04-27 15:59:35 +0200

    ✨ Games (#5)

--- src/main/java/fr/baptouk/pokerixe/backend/user/UserController.java
+++ src/main/java/fr/baptouk/pokerixe/backend/user/UserController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public final class UserController {
 
     @Autowired
     private UserService userService;
